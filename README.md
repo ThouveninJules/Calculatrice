909e11f1a7f51e1e3f6a7bae8ab5de44eb5d6cec
Jules Thouvenin <81535460+ThouveninJules@users.noreply.github.com>
2021-10-06 11:26:35 +0200

    La calculatrice finale

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Calculatrice
-On fait une calculatrice en c et on pleure.
+Calculatrice : 
+
+Entrez votre calcul après votre binaire (Exemple : ./main 2 + 3).
+
+Les opérants valides sont : 
+    + pour l'addition
+    - pour la soustraction
+    x pour la multiplication
+    : pour la division
+    "("...")" pour un calcul prioritaire (Les guillemets sont nécessaires !!!)
+
+Les règles de priorité ne s'appliquent pas seules, le calcul se fait de gauche à droite à l'exception des parenthèses.
+Le calcul n'a pas de limite sauf à l'intérieur des parenthèse où le calcul est limité à 2 opérateurs.
+
+Exemples : 
+    //Exemple valide
+    3 + "(" 6 x 7 ")" : 2 + 4 
+    = 3 + 42 : 2 + 4 
+    = 45 : 2 + 4 
+    = 22.5 + 4 
+    = 26.5
+
+    //Exemple d'erreur
+    3 + "(" 6 x 7 + 2 ")" : 2 + 4 
+    = Erreur : Le "+ 2" est en trop.
+
--- source/main.c
+++ source/main.c
@@ -1,80 +1,195 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void my_putchar(char c)
-{
-    write (1, &c, 1);
-}
-
-void my_putnbr(int nb){
-    if (nb < 0){
-        my_putchar('-');
-        nb = -nb;
-    }
-    if (nb <= 9){
-        my_putchar(nb+'0');
-    }else {
-        my_putnbr(nb / 10);
-        my_putchar(nb  % 10+'0');
-    }
-}
-
-int my_strlen(char* s){
-    int i = 0;
-    while(s[i]!='\0'){
-        i++;
-    }
-    return i;
-}
-
-void my_putstr(char* s){
-    for(int j=0; j<my_strlen(s); j++){
-        my_putchar(s[j]);
-    }
+float parent(float r, int o1, int o2, char op1, char op2){
+        if (op1 == '+') 
+        {
+            if (op2 == '+') 
+            {
+                o2 = o1 + o2;
+            }
+            else        
+            if (op2 == '-') 
+            {
+                o2 = o1 - o2;
+            }
+            else
+            if (op2 == 'x') 
+            {
+                o2 = o1 * o2;
+            }
+            else
+            if (op2 == ':') 
+            {
+                o2 = o1 / o2;
+            }
+            else
+            {
+                printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op1);
+                return 1;
+            }
+            r = r + o2;
+        }
+        else        
+        if (op1 == '-') 
+        {
+            if (op2 == '+') 
+            {
+                o2 = o1 + o2;
+            }
+            else        
+            if (op2 == '-') 
+            {
+                o2 = o1 - o2;
+            }
+            else
+            if (op2 == 'x') 
+            {
+                o2 = o1 * o2;
+            }
+            else
+            if (op2 == ':') 
+            {
+                o2 = o1 / o2;
+            }
+            else
+            {
+                printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op1);
+                return 1;
+            }
+            r = r - o2;
+        }
+        else
+        if (op1 == 'x') 
+        {
+            if (op2 == '+') 
+            {
+                o2 = o1 + o2;
+            }
+            else        
+            if (op2 == '-') 
+            {
+                o2 = o1 - o2;
+            }
+            else
+            if (op2 == 'x') 
+            {
+                o2 = o1 * o2;
+            }
+            else
+            if (op2 == ':') 
+            {
+                o2 = o1 / o2;
+            }
+            else
+            {
+                printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op1);
+                return 1;
+            }
+            r = r * o2;
+        }
+        else
+        if (op1 == ':') 
+        {
+            if (op2 == '+') 
+            {
+                o2 = o1 + o2;
+            }
+            else        
+            if (op2 == '-') 
+            {
+                o2 = o1 - o2;
+            }
+            else
+            if (op2 == 'x') 
+            {
+                o2 = o1 * o2;
+            }
+            else
+            if (op2 == ':') 
+            {
+                o2 = o1 / o2;
+            }
+            else
+            {
+                printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op2);
+                return 1;
+            }
+            r = r / o2;
+        }
+        else
+        {
+            printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op1);
+            return 1;
+        }
+    return r;
 }
 
 int main(int argc, const char * argv[]) {
-    //Variables
-    char operateur;
+    if(argc<4)
+    {
+        printf("Erreur : vous avez entrez moins de 2 operants separes d'un operateur.");
+        return 1;
+    }
+    char op;
     float o1;
     float o2;
     float resultat = 0.0;
-    
-    // Entrées
-    operateur = '+';
-    o1 = 3612;
-    o2 = 6;
-    scanf("%d", &o1);
+
+    o1 = atoi(argv[1]);
+    resultat = o1 + 0;
     
     // Calculs
-    if (operateur == '+') {
-        resultat = o1 + o2;
-        //printf("Le résultat de l'addition est: %.2f\n", resultat);
-        my_putstr("Le résultat de l'addition est : ");
-        my_putnbr(resultat);
+    for(int k =2; k<argc; k=k+2)
+    {
+        op = argv[k][0];
+        o2=atoi(argv[k+1]);
+
+        if(argv[k+1][0]=='(')
+        {
+            if(argv[k+5][0]!=')')
+            {
+                printf("Erreur : un calcul entre parentheses est limite a 2 operateurs.\nVeuillez vous referer au README pour l'utilisation de parentheses.");
+                return 1;
+            }else{
+            resultat = parent(resultat, atoi(argv[k+2]), atoi(argv[k+4]), op, argv[k+3][0]);
+            k=k+4;
+            }
+        }
+        else
+        {
+
+        if (op == '+') 
+        {
+            resultat = resultat + o2;
+        }
+        else        
+        if (op == '-') 
+        {
+            resultat = resultat - o2;
+        }
+        else
+        if (op == 'x') 
+        {
+            resultat = resultat * o2;
+        }
+        else
+        if (op == ':') 
+        {
+            resultat = resultat / o2;
+        }
+        else
+        {
+            printf("Erreur : L'operateur %c que vous avez entre n'est pas valide.\nVeuillez vous referer au README pour voir la liste des operateurs valides", op);
+            return 1;
+        }
+        }
+        
     }
-    
-    if (operateur == '-') {
-        resultat = o1 - o2;
-        // printf("Le résultat de la soustraction est: %.2f\n", resultat);
-        my_putstr("Le résultat de la soustraction est : ");
-        my_putnbr(resultat);
-    }
-    
-    if (operateur == '*') {
-        resultat = o1 * o2;
-        // printf("Le résultat de la multiplication est: %.2f\n", resultat);
-        my_putstr("Le résultat de la multiplication est : ");
-        my_putnbr(resultat);
-    }
-    
-    if (operateur == '/') {
-        resultat = o1 / o2;
-        // printf("Le résultat de la division est: %.2f\n", resultat);
-        my_putstr("Le résultat de la division est : ");
-        my_putnbr(resultat);
-    }
+    printf("Le resultat de votre operation est : %.2f\n", resultat);
     return 0;
 }
